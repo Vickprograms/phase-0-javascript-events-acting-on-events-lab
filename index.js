0f5ee52f8d27bb0f87f0ca217ac6fb5592d508d8
Victor <victormugo08@gmail.com>
2025-03-16 16:59:26 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function moveDodgerLeft() {
+    let left = parseInt(dodger.style.left, 10) || 0;
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    let left = parseInt(dodger.style.left, 10) || 0;
+    if (left < 360) { 
+      dodger.style.left = `${left + 10}px`;
+    }
+  }
+  
